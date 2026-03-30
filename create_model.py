cc4779e2278ad17dd0661334254a45eb91190e53
Pooja Puri <poojapuri1708@gmail.com>
2026-03-30 09:16:24 +0530

    Create create_model.py

---
+++ create_model.py
@@ -0,0 +1,28 @@
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+# Build CNN model
+model = Sequential()
+
+model.add(Conv2D(32, (3,3), activation='relu', input_shape=(48,48,1)))
+model.add(MaxPooling2D(2,2))
+
+model.add(Conv2D(64, (3,3), activation='relu'))
+model.add(MaxPooling2D(2,2))
+
+model.add(Conv2D(128, (3,3), activation='relu'))
+model.add(MaxPooling2D(2,2))
+
+model.add(Flatten())
+
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+
+model.add(Dense(7, activation='softmax'))  # 7 emotions
+
+# Compile model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Save model
+model.save(r"C:\Users\pooja puri\OneDrive\Documents\Desktop\emotion-detection-yolo\emotion_model.hdf5")
+print("✅ emotion_model.hdf5 created successfully!")
